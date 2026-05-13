319173e39b65c3ca0675baa3b2353810d3b756f4
Enzo Giacomelli <egiacomelli09@gmail.com>
2026-05-13 16:18:57 -0300

    criação do projeto para o front

---
+++ bookshop-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+
+  <div>
+    <h1>titulo teste</h1>
+    <p>
+      texto
+    </p>
+
+    <List>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+    </List>
+  </div>
+  
+</template>
+
+<style scoped></style>
